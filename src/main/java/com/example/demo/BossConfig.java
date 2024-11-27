8960d5f8a32450ad9a9379e8b9574a1cd675eeef
ASUS <ASUS@LAPTOP-3UHDAV6I>
2024-11-27 09:32:29 +0800

    split boss.java into smaller files for easy maintain

---
+++ src/main/java/com/example/demo/BossConfig.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+public class BossConfig {
+    public static final String IMAGE_NAME = "bossplane.png";
+    public static final double INITIAL_X_POSITION = 1000.0;
+    public static final double INITIAL_Y_POSITION = 400.0;
+    public static final double PROJECTILE_Y_OFFSET = 75.0;
+    public static final double FIRE_RATE = 0.04;
+    public static final double SHIELD_PROBABILITY = 0.002;
+    public static final int IMAGE_HEIGHT = 300;
+    public static final int VERTICAL_VELOCITY = 8;
+    public static final int HEALTH = 100;
+    public static final int MOVE_FREQUENCY = 5;
+    public static final int MAX_SAME_MOVE_FRAMES = 10;
+    public static final int Y_UPPER_BOUND = -100;
+    public static final int Y_LOWER_BOUND = 475;
+    public static final int MAX_SHIELD_FRAMES = 500;
+}
