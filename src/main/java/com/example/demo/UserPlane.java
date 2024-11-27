0b915530da92b339404480007965e7ef2b45850a
ASUS <ASUS@LAPTOP-3UHDAV6I>
2024-11-27 09:45:50 +0800

    split UserPlane.java into smaller files for easy maintain

--- src/main/java/com/example/demo/UserPlane.java
+++ src/main/java/com/example/demo/UserPlane.java
@@ -1,68 +1,67 @@
 package com.example.demo;
 
 public class UserPlane extends FighterPlane {
+    private int velocityMultiplier;
+    private int killCount;
 
-	private static final String IMAGE_NAME = "userplane.png";
-	private static final double Y_UPPER_BOUND = -40;
-	private static final double Y_LOWER_BOUND = 600.0;
-	private static final double INITIAL_X_POSITION = 5.0;
-	private static final double INITIAL_Y_POSITION = 300.0;
-	private static final int IMAGE_HEIGHT = 150;
-	private static final int VERTICAL_VELOCITY = 8;
-	private static final int PROJECTILE_X_POSITION = 110;
-	private static final int PROJECTILE_Y_POSITION_OFFSET = 20;
-	private int velocityMultiplier;
-	private int numberOfKills;
+    public UserPlane(int initialHealth) {
+        super(UserPlaneConfig.IMAGE_NAME, UserPlaneConfig.IMAGE_HEIGHT, UserPlaneConfig.INITIAL_X_POSITION, UserPlaneConfig.INITIAL_Y_POSITION, initialHealth);
+        this.velocityMultiplier = 0;
+        this.killCount = 0;
+    }
 
-	public UserPlane(int initialHealth) {
-		super(IMAGE_NAME, IMAGE_HEIGHT, INITIAL_X_POSITION, INITIAL_Y_POSITION, initialHealth);
-		velocityMultiplier = 0;
-	}
-	
-	@Override
-	public void updatePosition() {
-		if (isMoving()) {
-			double initialTranslateY = getTranslateY();
-			this.moveVertically(VERTICAL_VELOCITY * velocityMultiplier);
-			double newPosition = getLayoutY() + getTranslateY();
-			if (newPosition < Y_UPPER_BOUND || newPosition > Y_LOWER_BOUND) {
-				this.setTranslateY(initialTranslateY);
-			}
-		}
-	}
-	
-	@Override
-	public void updateActor() {
-		updatePosition();
-	}
-	
-	@Override
-	public ActiveActorDestructible fireProjectile() {
-		return new UserProjectile(PROJECTILE_X_POSITION, getProjectileYPosition(PROJECTILE_Y_POSITION_OFFSET));
-	}
+    @Override
+    public void updatePosition() {
+        if (!isMoving()) return;
 
-	private boolean isMoving() {
-		return velocityMultiplier != 0;
-	}
+        double initialY = getTranslateY();
+        moveVertically(UserPlaneConfig.VERTICAL_VELOCITY * velocityMultiplier);
 
-	public void moveUp() {
-		velocityMultiplier = -1;
-	}
+        if (isOutOfBounds()) {
+            setTranslateY(initialY); // Reset position if out of bounds
+        }
+    }
 
-	public void moveDown() {
-		velocityMultiplier = 1;
-	}
+    @Override
+    public void updateActor() {
+        updatePosition();
+    }
 
-	public void stop() {
-		velocityMultiplier = 0;
-	}
+    @Override
+    public ActiveActorDestructible fireProjectile() {
+        return new UserProjectile(UserPlaneConfig.PROJECTILE_X_POSITION, getProjectileY());
+    }
 
-	public int getNumberOfKills() {
-		return numberOfKills;
-	}
+    public void moveUp() {
+        velocityMultiplier = -1;
+    }
 
-	public void incrementKillCount() {
-		numberOfKills++;
-	}
+    public void moveDown() {
+        velocityMultiplier = 1;
+    }
 
+    public void stop() {
+        velocityMultiplier = 0;
+    }
+
+    public int getKillCount() {
+        return killCount;
+    }
+
+    public void incrementKillCount() {
+        killCount++;
+    }
+
+    private boolean isMoving() {
+        return velocityMultiplier != 0;
+    }
+
+    private boolean isOutOfBounds() {
+        double newY = getLayoutY() + getTranslateY();
+        return newY < UserPlaneConfig.Y_UPPER_BOUND || newY > UserPlaneConfig.Y_LOWER_BOUND;
+    }
+
+    private double getProjectileY() {
+        return getLayoutY() + getTranslateY() + UserPlaneConfig.PROJECTILE_Y_OFFSET;
+    }
 }
---
+++ src/main/java/com/example/demo/UserPlaneConfig.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+public class UserPlaneConfig {
+    public static final String IMAGE_NAME = "userplane.png";
+    public static final double Y_UPPER_BOUND = -40.0;
+    public static final double Y_LOWER_BOUND = 600.0;
+    public static final double INITIAL_X_POSITION = 5.0;
+    public static final double INITIAL_Y_POSITION = 300.0;
+    public static final int IMAGE_HEIGHT = 150;
+    public static final int VERTICAL_VELOCITY = 8;
+    public static final int PROJECTILE_X_POSITION = 110;
+    public static final int PROJECTILE_Y_OFFSET = 20;
+}
