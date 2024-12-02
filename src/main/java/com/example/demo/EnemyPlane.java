be44fd7c6cb18071f855bdae30d079497ec23322
Tan Jia Hor <jiahortan1@gmail.com>
2024-12-02 22:08:39 +0800

    split enemyplane into smaller files

--- src/main/java/com/example/demo/EnemyPlane.java
+++ src/main/java/com/example/demo/EnemyPlane.java
@@ -1,37 +1,34 @@
 package com.example.demo;
 
 public class EnemyPlane extends FighterPlane {
+    private final EnemyPlaneConfig config;
 
-	private static final String IMAGE_NAME = "enemyplane.png";
-	private static final int IMAGE_HEIGHT = 150;
-	private static final int HORIZONTAL_VELOCITY = -6;
-	private static final double PROJECTILE_X_POSITION_OFFSET = -100.0;
-	private static final double PROJECTILE_Y_POSITION_OFFSET = 50.0;
-	private static final int INITIAL_HEALTH = 1;
-	private static final double FIRE_RATE = .01;
+    public EnemyPlane(double initialXPos, double initialYPos) {
+        super(EnemyPlaneConfig.IMAGE_NAME, 
+              EnemyPlaneConfig.IMAGE_HEIGHT, 
+              initialXPos, 
+              initialYPos, 
+              EnemyPlaneConfig.HEALTH);
+        this.config = new EnemyPlaneConfig();
+    }
 
-	public EnemyPlane(double initialXPos, double initialYPos) {
-		super(IMAGE_NAME, IMAGE_HEIGHT, initialXPos, initialYPos, INITIAL_HEALTH);
-	}
+    @Override
+    public void updatePosition() {
+        moveHorizontally(EnemyPlaneConfig.HORIZONTAL_VELOCITY);
+    }
 
-	@Override
-	public void updatePosition() {
-		moveHorizontally(HORIZONTAL_VELOCITY);
-	}
+    @Override
+    public ActiveActorDestructible fireProjectile() {
+        if (Math.random() < EnemyPlaneConfig.FIRE_RATE) {
+            double projectileXPosition = getProjectileXPosition(EnemyPlaneConfig.PROJECTILE_X_OFFSET);
+            double projectileYPosition = getProjectileYPosition(EnemyPlaneConfig.PROJECTILE_Y_OFFSET);
+            return new EnemyProjectile(projectileXPosition, projectileYPosition);
+        }
+        return null;
+    }
 
-	@Override
-	public ActiveActorDestructible fireProjectile() {
-		if (Math.random() < FIRE_RATE) {
-			double projectileXPosition = getProjectileXPosition(PROJECTILE_X_POSITION_OFFSET);
-			double projectileYPosition = getProjectileYPosition(PROJECTILE_Y_POSITION_OFFSET);
-			return new EnemyProjectile(projectileXPosition, projectileYPosition);
-		}
-		return null;
-	}
-
-	@Override
-	public void updateActor() {
-		updatePosition();
-	}
-
+    @Override
+    public void updateActor() {
+        updatePosition();
+    }
 }
---
+++ src/main/java/com/example/demo/EnemyPlaneConfig.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+public class EnemyPlaneConfig {
+    public static final String IMAGE_NAME = "enemyplane.png";
+    public static final double INITIAL_X_POSITION = 800.0; 
+    public static final double INITIAL_Y_POSITION = 300.0; 
+    public static final double PROJECTILE_X_OFFSET = -100.0;
+    public static final double PROJECTILE_Y_OFFSET = 50.0;
+    public static final double FIRE_RATE = 0.01;
+    public static final int IMAGE_HEIGHT = 150;
+    public static final int HORIZONTAL_VELOCITY = -6;
+    public static final int HEALTH = 1;
+}
