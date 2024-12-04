2b6591e1c98d3911522fbb0a6b16c5fe5a94cd79
Tan Jia Hor <jiahortan1@gmail.com>
2024-12-04 11:14:22 +0800

    make minor adjustment to levelparent

--- src/main/java/com/example/demo/levels/LevelParent.java
+++ src/main/java/com/example/demo/levels/LevelParent.java
@@ -6,7 +6,7 @@ import com.example.demo.UserPlane;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Observable;
-import java.util.stream.Collectors;
+import java.util.stream.Collectors; 
 
 import javafx.animation.*;
 import javafx.event.EventHandler;
