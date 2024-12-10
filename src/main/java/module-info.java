367637cf7a45f0996fa264ff08bc898a2895d67c
Tan Jia Hor <jiahortan1@gmail.com>
2024-12-10 16:32:53 +0800

    forgot to push this

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,5 +7,11 @@ module com.example.demo {
     opens com.example.demo.levels to javafx.fxml;
     opens com.example.demo.image to javafx.fxml;
     opens com.example.demo.projectile to javafx.fxml;
+    exports com.example.demo.actor;
+    exports com.example.demo.levels;
+    exports com.example.demo.image;
+    exports com.example.demo.projectile;
+    exports com.example.demo.config;
+    exports com.example.demo.input;
     exports com.example.demo.controller;
 }
