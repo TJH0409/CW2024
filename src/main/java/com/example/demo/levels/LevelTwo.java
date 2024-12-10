888da5cc68a82a65ad67b3ab5cf8fe0ed11a1d54
Tan Jia Hor <jiahortan1@gmail.com>
2024-12-10 16:02:01 +0800

    Added a new level with new, stronger enemy

--- src/main/java/com/example/demo/levels/LevelTwo.java
+++ src/main/java/com/example/demo/levels/LevelTwo.java
@@ -1,18 +1,18 @@
 package com.example.demo.levels;
 
 import com.example.demo.actor.ActiveActorDestructible;
-import com.example.demo.actor.EnemyPlane;
+import com.example.demo.actor.EnemyPlane2;
 
 /**
  * Represents the first level of the game with specific enemy spawning and level progression mechanics.
  * Extends LevelParent to implement level-specific behaviors for Level One.
  */
-public class LevelOne extends LevelParent {
+public class LevelTwo extends LevelParent {
 	
 	private static final String BACKGROUND_IMAGE_NAME = "/com/example/demo/images/background1.jpg";
-	private static final String NEXT_LEVEL = "com.example.demo.levels.LevelTwo";
+	private static final String NEXT_LEVEL = "com.example.demo.levels.LevelBoss";
 	private static final int TOTAL_ENEMIES = 5;
-	private static final int KILLS_TO_ADVANCE = 10;
+	private static final int KILLS_TO_ADVANCE = 15;
 	private static final double ENEMY_SPAWN_PROBABILITY = .20;
 	private static final int PLAYER_INITIAL_HEALTH = 5;
 
@@ -23,7 +23,7 @@ public class LevelOne extends LevelParent {
      * @param screenHeight Total height of the game screen
      * @param screenWidth Total width of the game screen
      */
-	public LevelOne(double screenHeight, double screenWidth) {
+	public LevelTwo(double screenHeight, double screenWidth) {
 		super(BACKGROUND_IMAGE_NAME, screenHeight, screenWidth, PLAYER_INITIAL_HEALTH);
 	}
 
@@ -59,7 +59,7 @@ public class LevelOne extends LevelParent {
 		for (int i = 0; i < TOTAL_ENEMIES - currentNumberOfEnemies; i++) {
 			if (Math.random() < ENEMY_SPAWN_PROBABILITY) {
 				double newEnemyInitialYPosition = Math.random() * getEnemyMaximumYPosition();
-				ActiveActorDestructible newEnemy = new EnemyPlane(getScreenWidth(), newEnemyInitialYPosition);
+				ActiveActorDestructible newEnemy = new EnemyPlane2(getScreenWidth(), newEnemyInitialYPosition);
 				addEnemyUnit(newEnemy);
 			}
 		}
